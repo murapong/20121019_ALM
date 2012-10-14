05d079496e3fab27e2f44524e70cdca4b1b91d40
murapong <murapong@gmail.com>
2012-10-14 14:37:20 +0900

    プロジェクトの雛形を作成

---
+++ 20121019_ALM/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  20121019_ALM
+//
+//  Created by murapong on 12/10/14.
+//  Copyright (c) 2012年 com.murapong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ 20121019_ALM/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  20121019_ALM
+//
+//  Created by murapong on 12/10/14.
+//  Copyright (c) 2012年 com.murapong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
